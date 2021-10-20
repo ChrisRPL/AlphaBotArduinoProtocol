325daa4e307acb14dc039e7ff7a26023ce94b96b
SolveItProject <69324227+SolveItProject@users.noreply.github.com>
2021-10-20 21:08:10 +0200

    add main class frame and header class

---
+++ RobotProtocol.h
@@ -0,0 +1,31 @@
+class RobotProtocol
+{
+public:
+    RobotProtocol();
+    void moveForward(int distance);
+    void moveBackward(int distance);
+    void rotateRight(int milis);
+    void rotateLeft(int milis);
+    void stop();
+    void setSpeed(char speed);
+    int getSonarValue();
+    int getInfraredSensorValue();
+
+private:
+    int rightSpeedWheelPin;
+    int rightForwardWheelPin;
+    int rightBackwardWheelPin;
+
+    int leftSpeedWheelPin;
+    int leftForwardWheelPin;
+    int leftBackwardWheelPin;
+
+    static char leftWheelSpeed, rightWheelSpeed;
+
+    void leftWheelForward(char speed);
+    void leftWheelBackward(char speed);
+    void leftWheelStop();
+    void rightWheelForward(char speed);
+    void rightWheelBackward(char speed);
+    void rightWheelStop();
+}
